c5ed96fc7ad0e8db44b5ad0a64b2f0387dce49d4
Azam Ahmed <aziskool20@gmail.com>
2021-10-15 16:41:36 -0400

    Solved Plus Minus problem

---
+++ PlusMinus.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  int positiveCount = 0;
+  int negativeCount = 0;
+  int zeroCount = 0;
+
+  int N;
+  cin >> N;
+
+  int arr[N];
+
+  for (int i = 0; i < N; i++)
+  {
+    int x;
+    cin >> x;
+    arr[i] = x;
+
+    if (arr[i] == 0)
+      zeroCount++;
+    if (arr[i] > 0)
+      positiveCount++;
+    if (arr[i] < 0)
+      negativeCount++;
+  }
+
+  double positiveRatio = (double)positiveCount / N;
+  double negativeRatio = (double)negativeCount / N;
+  double zeroRatio = (double)zeroCount / N;
+
+  cout << fixed << setprecision(6) << positiveRatio << endl;
+  cout << fixed << setprecision(6) << negativeRatio << endl;
+  cout << fixed << setprecision(6) << zeroRatio << endl;
+}

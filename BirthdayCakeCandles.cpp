c77fab03cf32881c2fb067ecbd5e6189c119917f
Azam Ahmed <aziskool20@gmail.com>
2021-10-16 23:49:16 -0400

    Solved Birthday Cake Candles problem

---
+++ BirthdayCakeCandles.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  int arr[N];
+  for (int i = 0; i < N; i++) {
+    cin >> arr[i];
+  }
+
+  sort(arr, arr + N);
+
+  int maxValue = arr[N - 1];
+  int maxCount = 0;
+
+  int i = N - 1;
+  while (arr[i] == maxValue) {
+    maxCount++;
+    i--;
+  }
+
+  // Going through each value is too slow!
+  // for (int i = 0; i < N; i++) {
+  //   if (arr[i] == maxValue) {
+  //     maxCount++;
+  //   }
+  // }
+
+  cout << maxCount << endl;
+}

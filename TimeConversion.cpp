4921e5006a96b2c050a1c3cde66910641b8bfc46
Azam Ahmed <aziskool20@gmail.com>
2021-10-18 15:19:07 -0400

    Solved Time Conversion problem

---
+++ TimeConversion.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string time;
+  cin >> time;
+
+  int length = time.length();
+
+  string converted = time.substr(0, length - 2);
+  string ending = time.substr(length - 2, length);
+
+  int frontValue = stoi(time.substr(0, 2));
+  string convertedFrontValue;
+  if (ending == "PM" && frontValue != 12)
+  {
+    frontValue += 12;
+  }
+  else if (ending == "AM" && frontValue == 12)
+  {
+    frontValue -= 12;
+  }
+
+  convertedFrontValue = to_string(frontValue);
+  
+  if (frontValue < 10) {
+    convertedFrontValue.insert(0, "0");
+  }
+
+  converted.replace(0, 2, convertedFrontValue);
+
+  cout << converted << endl;
+}
